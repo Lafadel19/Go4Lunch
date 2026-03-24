8dc47d1f36c558275199a85034e5ed589daffac9
Lucas Canu <Lucas.canu27@gmail.com>
2026-03-24 16:16:36 +0100

    add : LoginActivity

---
+++ app/src/main/java/go4lunch/ui/view/LoginActivity.java
@@ -0,0 +1,14 @@
+package go4lunch.ui.view;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.Go4Lunch.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
